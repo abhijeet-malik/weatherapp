0e596b443d55ea676f8241f9beb2c4ee4cdc71ca
abhijeet-malik <abhijeetmalik4321@gmail.com>
2025-06-22 00:20:31 +0530

    Add wind icon image

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import windIcon from '../assets/wind.png';
 import humidityIcon from "../assets/humidity.png";
 import visibilityIcon from "../assets/visibility.png";
-import sunriseIcon from "../assets/sunrise.png";
+import sunriseIcon from "../assets/Sunrise.png";
 import sunsetIcon from "../assets/sunset.png";
 
 const Icon = ({ src, alt, className }) => (
